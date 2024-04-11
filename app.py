71f946004d9b65b0f0578af8ae4dbc2939b4d9fa
EricWithC04 <negativegame117@gmail.com>
2024-04-11 20:08:51 -0300

    comments

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import MySQLdb
 
+# Conectamos con la base de datos
 try:
     db = MySQLdb.Connect(host='localhost', user='root', password='', db='localidadesdb')
 except MySQLdb.Error as err:
@@ -9,6 +10,7 @@ except MySQLdb.Error as err:
     sys.exit(1)
 print("Conexión correcta.")
 
+# Leemos el archivo CSV y le damos formato de diccionario
 with open('localidades.csv') as archivo_csv:
     lector_csv = csv.reader(archivo_csv, delimiter=',', quotechar='"')
     cabecera = next(lector_csv)
@@ -23,6 +25,7 @@ with open('localidades.csv') as archivo_csv:
 
 cursor = db.cursor()
 
+# Todas las consultas para la base de datos
 create_table_localidades = "CREATE TABLE localidades (provincia VARCHAR(255), id INT, localidad VARCHAR(255), cp VARCHAR(10), id_prov_mstr INT)"
 
 select_all_provinces = "SELECT provincia AS 'Provincia', COUNT(*) AS 'Localidades' FROM `localidades` GROUP BY provincia"
@@ -34,25 +37,31 @@ insert_localidades = 'INSERT INTO localidades (provincia, id, localidad, cp, id_
 select_localidades = 'SELECT * FROM localidades WHERE provincia = %s'
 
 try:
+    # Si la tabla ya existe, la borramos y la volvemos a crear
     cursor.execute(drop_if_exists)
     cursor.execute(create_table_localidades)
     print("Tabla creada con exito.")
 
+    # Insertamos todos los registros de localidades
     for loc in localidades:
         valores = [loc["provincia"], loc["id"], loc["localidad"], loc["cp"], loc["id_prov_mstr"]]
         cursor.execute(insert_localidades, valores)
     db.commit()
     print("Registros insertados con exito.")
 
+    # Traemos todas las provincias junto con la cantidad de localidades de cada una
     cursor.execute(select_all_provinces)
     provinces_localities = dict(cursor.fetchall())
 
     all_provinces = list(provinces_localities.keys())
 
+    # Traemos todas las localidades de cada una de las provincias
     for prov in all_provinces:
         cursor.execute(select_localidades, [prov])
         locs = list(cursor.fetchall())
 
+        # Escribimos los resultados en un archivo CSV para cada una de las provincias, 
+        # primero la cabecera, todas las localidades y por ultimo el total de localidades
         with open(f'localidades_provincias/{prov}.csv', mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(cabecera)
